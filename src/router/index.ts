79cea5dd6bf8f31d4517bd13b85c58da7e7666c5
周子皓 <1392676693@qq.com>
2024-11-22 09:41:39 +0800

    完成reviewlist和部分Profile

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import MainLayout from "@/layouts/MainLayout.vue";
-import LatestCommentsList from "@/pages/LatestCommentsList.vue";
+import LatestCommentsList from "@/pages/LatestReviewList.vue";
 import CourseList from "@/pages/CourseList.vue";
 
 const routes = [
